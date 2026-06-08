77b605b82e0f583ee06b7251e25b8ac673d72a5d
hugocoosmel <hucoslu1@protonmail.com>
2026-06-08 02:10:46 +0200

    Añadida generación de clave pública.

--- crypto_utils.py
+++ crypto_utils.py
@@ -3,16 +3,17 @@ from cryptography.hazmat.primitives import serialization
 
 class Clave:
     def __init__(self):
-        self.__public_key = None
-        self.__private_key = self.obtain_private_key()
+        self.__private_key = self.__generate_private_key()
+        self.__public_key = self.__generate_public_key(self.__private_key)
+
 
     @property
     def public_key(self):
-        return self.__public_key
+        return self.__serialize_public_key(self.__public_key)
 
     @property
     def private_key(self):
-        return self.__private_key
+        return self.__serialize_private_key(self.__private_key)
 
     # Method that creates the private key
     @staticmethod
@@ -26,20 +27,38 @@ class Clave:
 
     # Method that serializes the private key
     @staticmethod
-    def __serialize_private(key):
+    def __serialize_private_key(key):
         pem = key.private_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PrivateFormat.PKCS8,
             encryption_algorithm=serialization.NoEncryption()
         )
-
         return pem
 
     def obtain_private_key(self):
-        return self.__serialize_private(self.__generate_private_key())
+        return self.__serialize_private_key(self.__generate_private_key())
+
+    @staticmethod
+    def __generate_public_key(private_key):
+        public_key = private_key.public_key()
+        return public_key
+
+    @staticmethod
+    def __serialize_public_key(key):
+        pem = key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        return pem
+
+    def obtain_public_key(self, private_key):
+        return self.__serialize_public_key(self.__generate_public_key(private_key))
 
 
 if __name__ == '__main__':
     clave = Clave()
     with open('private_key.pem', 'wb') as f:
+        f.write(clave.private_key)
+
+    with open('public_key.pem', 'wb') as f:
         f.write(clave.private_key)
